96db43a08e27112e38ec45ca06ba87a9db8a31eb
ryanmseefeldt <ryanmseefeldt@gmail.com>
2025-04-09 17:54:07 -0400

    Implement calculator functions and corresponding tests

--- test_calculator.py
+++ test_calculator.py
@@ -13,30 +13,30 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 2)
-        self.assertEqual(subtract(-1, -1), 0)
-        self.assertEqual(subtract(0, 5), -5)
+        self.assertEqual(sub(5, 3), 2)
+        self.assertEqual(sub(-1, -1), 0)
+        self.assertEqual(sub(0, 5), -5)
     ##########################
 
     ######## Partner 1
     def test_multiply(self):
-        self.assertEqual(multiply(2, 3), 6)
-        self.assertEqual(multiply(-1, 5), -5)
-        self.assertEqual(multiply(0, 100), 0)
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(-1, 5), -5)
+        self.assertEqual(mul(0, 100), 0)
 
     def test_divide(self):
-        self.assertEqual(divide(2, 10), 5)
-        self.assertEqual(divide(-2, -10), 5)
-        self.assertEqual(divide(5, 25), 5)
+        self.assertEqual(div(2, 10), 5)
+        self.assertEqual(div(-2, -10), 5)
+        self.assertEqual(div(5, 25), 5)
 
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            logarithm(2, -5)
+            log(2, -5)
 
     def test_hypotenuse(self):
-        self.assertAlmostEqual(hypotenuse(3, 4), 5)
-        self.assertAlmostEqual(hypotenuse(5, 12), 13)
-        self.assertAlmostEqual(hypotenuse(8, 15), 17)
+        self.assertAlmostEqual(hypo(3, 4), 5)
+        self.assertAlmostEqual(hypo(5, 12), 13)
+        self.assertAlmostEqual(hypo(8, 15), 17)
 
     def test_sqrt(self):
         self.assertAlmostEqual(square_root(4), 2)
@@ -48,16 +48,16 @@ class TestCalculator(unittest.TestCase):
     ######## Partner 2
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            divide(0, 5)
+            div(0, 5)
 
     def test_logarithm(self):
-        self.assertAlmostEqual(logarithm(2, 8), 3)
-        self.assertAlmostEqual(logarithm(10, 100), 2)
-        self.assertAlmostEqual(logarithm(4, 16), 2)
+        self.assertAlmostEqual(log(2, 8), 3)
+        self.assertAlmostEqual(log(10, 100), 2)
+        self.assertAlmostEqual(log(4, 16), 2)
 
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            logarithm(1, 5)
+            log(1, 5)
     ##########################
 
 # Do not touch this
