174ca65b2171bed85683d5045f3422f688212bf6
ryanmseefeldt <ryanmseefeldt@gmail.com>
2025-04-09 22:48:41 -0400

    Implement calculator functions and corresponding tests

--- calculator.py
+++ calculator.py
@@ -1,11 +1,6 @@
 #https://github.com/ryanmseefeldt/lab10-RS.git
 #Partner 1: Ryan
 #Partner 2: Seefeldt
-
-'''calculator.py
-- Defines functions used to create a simple calculator
-'''
-
 import math
 
 def square_root(a):
@@ -13,29 +8,29 @@ def square_root(a):
         raise ValueError("Cannot take square root of negative number")
     return math.sqrt(a)
 
-def hypo(a, b):
+def hypotenuse(a, b):
     return math.hypot(a, b)
 
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return b / a
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Invalid input for logarithm")
     return math.log(b, a)
 
-def exp(a, b):
+def exponential(a, b):
     return a ** b
 
 
--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,65 @@
-# https://github.com/ryanmseefeldt/lab10-RS.git
-# Partner 1: Ryan
-# Partner 2: Seefeldt
+#https://github.com/ryanmseefeldt/lab10-RS.git
+#Partner 1: Ryan
+#Partner 2: Seefeldt
+'''class TestCalculator(unittest.TestCase):
+    ######### Partner 2
+    # def test_add(self): # 3 assertions
+    #     fill in code
 
+    # def test_subtract(self): # 3 assertions
+    #     fill in code
+    # ##########################
+
+    ######## Partner 1
+    # def test_multiply(self): # 3 assertions
+    #     fill in code
+
+    # def test_divide(self): # 3 assertions
+    #     fill in code
+    # ##########################
+
+    ######## Partner 2
+    # def test_divide_by_zero(self): # 1 assertion
+    #     # call division function inside, example:
+    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
+    #     #     div(0, 5)
+    #     fill in code
+
+    # def test_logarithm(self): # 3 assertions
+    #     fill in code
+
+    # def test_log_invalid_base(self): # 1 assertion
+    #     # use same technique from test_divide_by_zero
+    #     fill in code
+    # ##########################
+
+    ######## Partner 1
+    # def test_log_invalid_argument(self): # 1 assertion
+    #     # call log function inside, example:
+    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
+    #     #     logarithm(0, 5)
+    #     fill in code
+
+    # def test_hypotenuse(self): # 3 assertions
+    #     fill in code
+
+    # def test_sqrt(self): # 3 assertions
+    #     # Test for invalid argument, example:
+    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
+    #     #    square_root(NUM)
+    #     # Test basic function
+    #     fill in code
+    ##########################
+
+# Do not touch this
+if __name__ == "__main__":
+    unittest.main()'''
 import unittest
 from calculator import *
+
+# https://github.com/ryanmseefeldt/lab10-RS.git
+# Partner 1: Ryan
+# Partner 2: Seefeld
 
 class TestCalculator(unittest.TestCase):
     ######## Partner 2
@@ -13,30 +69,30 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self):
-        self.assertEqual(sub(5, 3), 2)
-        self.assertEqual(sub(-1, -1), 0)
-        self.assertEqual(sub(0, 5), -5)
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(-1, -1), 0)
+        self.assertEqual(subtract(0, 5), -5)
     ##########################
 
     ######## Partner 1
     def test_multiply(self):
-        self.assertEqual(mul(2, 3), 6)
-        self.assertEqual(mul(-1, 5), -5)
-        self.assertEqual(mul(0, 100), 0)
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-1, 5), -5)
+        self.assertEqual(multiply(0, 100), 0)
 
     def test_divide(self):
-        self.assertEqual(div(2, 10), 5)
-        self.assertEqual(div(-2, -10), 5)
-        self.assertEqual(div(5, 25), 5)
+        self.assertEqual(divide(2, 10), 5)
+        self.assertEqual(divide(-2, -10), 5)
+        self.assertEqual(divide(5, 25), 5)
 
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            log(2, -5)
+            logarithm(2, -5)
 
     def test_hypotenuse(self):
-        self.assertAlmostEqual(hypo(3, 4), 5)
-        self.assertAlmostEqual(hypo(5, 12), 13)
-        self.assertAlmostEqual(hypo(8, 15), 17)
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(hypotenuse(8, 15), 17)
 
     def test_sqrt(self):
         self.assertAlmostEqual(square_root(4), 2)
@@ -48,16 +104,16 @@ class TestCalculator(unittest.TestCase):
     ######## Partner 2
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            div(0, 5)
+            divide(0, 5)
 
     def test_logarithm(self):
-        self.assertAlmostEqual(log(2, 8), 3)
-        self.assertAlmostEqual(log(10, 100), 2)
-        self.assertAlmostEqual(log(4, 16), 2)
+        self.assertAlmostEqual(logarithm(2, 8), 3)
+        self.assertAlmostEqual(logarithm(10, 100), 2)
+        self.assertAlmostEqual(logarithm(4, 16), 2)
 
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            log(1, 5)
+            logarithm(1, 5)
     ##########################
 
 # Do not touch this
